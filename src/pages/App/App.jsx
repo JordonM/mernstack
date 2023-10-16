c4a8c2f9946e809d255bc2a012ffaffd7d504b00
Jordon <jordonmeril9@gmail.com>
2023-10-15 20:38:14 -0400

    initial commits for lab

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import { useState } from 'react';
+import AuthPage from '../AuthPage/AuthPage';
+import NewOrderPage from '../NewOrderPage/NewOrderPage';
+import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
+
+export default function App() {
+  const [user, setUser] = useState(null)
+
+  return (
+    <main className="App">
+      { user ?
+        <NewOrderPage />
+        :
+        <AuthPage />
+      }
+    </main>
+  );
+}
+
---
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -0,0 +1,5 @@
+export default function OrderPageHistory() {
+    return (
+        <h1>OrderPageHistory</h1>
+    )
+}
